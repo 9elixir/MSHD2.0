caf3e93ea6df910d471cae1550745f0510d2e16a
G2021lyq <65072383+G2021lyq@users.noreply.github.com>
2023-12-19 22:08:39 +0800

    数据分析

--- src/main/java/com/qiu/service/GeoCodeInfoServiceImpl.java
+++ src/main/java/com/qiu/service/GeoCodeInfoServiceImpl.java
@@ -5,8 +5,10 @@ import com.qiu.pojo.geo_code_info;
 import com.qiu.pojo.geo_code_infoExample;
 import com.qiu.service.GeoCodeInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -70,7 +72,101 @@ public class GeoCodeInfoServiceImpl implements GeoCodeInfoService {
         return mapper.updateByPrimaryKey(record);
     }
 
+    @Override
+    public List<geo_code_info> getAllGeoCodeInfo() {
+        return selectByExample(new geo_code_infoExample());
+    }
+
+    @Override
+    // 获取所有不重复的城市列表
+    public List<String> getAllCities() {
+        List<String> cities = new ArrayList<>();
+        // 获取所有geo_code_info记录
+        List<geo_code_info> geoCodeInfoList = getAllGeoCodeInfo();
+        // 遍历记录，将城市添加到List中
+        for (geo_code_info geoCodeInfo : geoCodeInfoList) {
+            String city = geoCodeInfo.getProvince();
+            if (city != null && !city.isEmpty() && !cities.contains(city)) {
+                cities.add(city);
+            }
+        }
+        return cities;
+    }
+
+    @Override
+    public List<geo_code_info> selectByCity(String city){
+        geo_code_infoExample example = new geo_code_infoExample();
+        geo_code_infoExample.Criteria criteria = example.createCriteria();
+        criteria.andProvinceEqualTo(city);
+        return selectByExample(example);
+    }
+
+    @Override
+    public geo_code_info minGeo(List<geo_code_info> list){
+        geo_code_info code = list.get(0);
+        for (geo_code_info B : list) {
+            if (B.getGeoCode().compareTo(code.getGeoCode()) <  0)
+                code = B;
+        }
+        return code;
+    }
+
+    @Override
+    public geo_code_info maxGeo(List<geo_code_info> list){
+        geo_code_info code = list.get(0);
+        for (geo_code_info B : list) {
+            if (B.getGeoCode().compareTo(code.getGeoCode()) >  0)
+                code = B;
+        }
+        return code;
+    }
+
+    private static List<String> cityTable = null;//映射编号和地区
+    private static List<geo_code_info> firstGeoCode = null;//映射第一个编号
+    private static List<geo_code_info> lastGeoCode = null;//映射最后一个编号
+    private static Boolean flag=false;
+
+    @Override
+    public void init() {
+        if (flag) return ;
+        flag=true;
+        firstGeoCode = new ArrayList<>();
+        lastGeoCode = new ArrayList<>();
+
+        cityTable = getAllCities();
+        for (String city : cityTable) {
+            List<geo_code_info> list = selectByCity(city);
+            firstGeoCode.add(minGeo(list));
+            lastGeoCode.add(maxGeo(list));
+        }
+        // 可以继续添加其他映射关系
+    }
+
+    @Override
+    public void printOut(){
+        for (int i=0;i<cityTable.size();i++){
+            System.out.println(cityTable.get(i));
+            System.out.println(firstGeoCode.get(i));
+            System.out.println(lastGeoCode.get(i));
+        }
+    }
+
+    @Override
+    public String getCity(int i){
+        return cityTable.get(i);
+    }
+    @Override
+    public geo_code_info getFirstGeoCode(int i){
+        return firstGeoCode.get(i);
+    }
+    @Override
+    public geo_code_info getLastGeoCode(int i){
+        return lastGeoCode.get(i);
+    }
+
     public void setMapper(geo_code_infoMapper mapper) {
         this.mapper = mapper;
     }
+
+
 }
---
+++ src/main/webapp/WEB-INF/jsp/lineChart.jsp
@@ -0,0 +1,18 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Line Chart</title>
+</head>
+<body>
+<h1>Line Chart</h1>
+
+<%-- Display the Line Chart using Base64 image --%>
+<img src="data:image/png;base64,${lineChartBase64}" alt="Line Chart">
+
+</body>
+</html>
+
